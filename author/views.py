9d293d1fc1cd6fdec52d37249fe34d5f4f30bb7e
Leo Leite <leomagalhaes2008@gmail.com>
2017-10-10 22:11:44 +0000

    Create view, form and template

--- author/views.py
+++ author/views.py
@@ -1,5 +1,12 @@
 from my_blog import app
+from flask import render_template, redirect
+from author.form import RegisterForm
 
 @app.route('/login')
 def login():
     return "Login Successfull"
+    
+@app.route('/register', methods = ('GET', 'POST'))
+def register():
+    form = RegisterForm()
+    return render_template('author/register.html', form=form)
