cad6fc8d038554652d222ece6c47c570ac912862
Leo Leite <leomagalhaes2008@gmail.com>
2017-10-10 18:17:08 +0000

    ORM working with database :D

--- README.txt
+++ README.txt
@@ -1,9 +1,7 @@
-Tried 
-
 $ python manage.py shell
 
     >>> from my_blog import db
     >>> from author.models import *
     >>> db.create_all()
 
-but the changes doesn't reflect on the database cli
+works now. found typo in constant DB_URI
--- settings.py
+++ settings.py
@@ -6,8 +6,10 @@ DEBUG=True
 DB_USERNAME='magal36'
 DB_PASSWORD=''
 BLOG_DATABASE_NAME='blog'
+
 #cloud9 host config
 DB_HOST=os.getenv('IP','0.0.0.0')
 DB_URI = "mysql+pymysql://%s:%s@%s/%s" % (DB_USERNAME, DB_PASSWORD, DB_HOST, BLOG_DATABASE_NAME)
-SLQALCHEMY_DATABASE_URI = DB_URI
+SQLALCHEMY_DATABASE_URI = DB_URI
+#SQLALCHEMY_DATABASE_URI = "sqlite:///home/ubuntu/workspace/blog.db"
 SQLAlCHEMY_TRACK_MODIFICATIONS = True
