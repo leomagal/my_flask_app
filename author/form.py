3542c2bd1059ef5d63261f35db3911029e4cd88f
Leo Leite <leomagalhaes2008@gmail.com>
2017-10-11 20:46:54 +0000

    Fix form validators using absolute imports structure. Don't really know why doesn't work the other way

--- author/form.py
+++ author/form.py
@@ -1,22 +1,32 @@
-from flask_wtf import Form
-from wtforms import validators, StringField, PasswordField
+from flask_wtf import Form as FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms import validators
+from wtforms.validators import Length, DataRequired, EqualTo
 from wtforms.fields.html5 import EmailField
 
-class RegisterForm(Form):
-    fullname = StringField('Full Name', [validators.Required()])
-    email = EmailField('Email', [validators.Required()])
+class RegisterForm(FlaskForm):
+    fullname = StringField('Full Name', [validators.DataRequired()])
+    email = EmailField('Email', [validators.DataRequired()])
+#    username = StringField('username',
+#        [validators.Required()
+#        , validators.Lenght(min=4, max=25)
+#        ])
     username = StringField('username',
-        [validators.Required()
-        #, validators.Lenght(min=4, max=25)
-        ])
+        validators = [DataRequired(), Length(min=4, max=25)])
+
+#    password = PasswordField('New Password',
+#        [validators.DataRequired(),
+#        validators.EqualTo('confirm', 
+#        message = 'Passwords muyst match')
+#        , validators.Lenght(min=4, max=80)
+#        ])
+
     password = PasswordField('New Password',
-        [validators.Required(),
-        validators.EqualTo('confirm', 
-        message = 'Passwords muyst match')
-        #, validators.Lenght(min=4, max=80)
-        ])
+        validators = [DataRequired(),
+        EqualTo('confirm', message='Passwords must match'),
+        Length(min=4,max=80)])
     confirm = PasswordField('Repeat Password', 
-        [validators.Required()])
+        [validators.DataRequired()])
         
         
     
