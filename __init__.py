5c7bf4e1e5a2f0f1ca21bef520a2028e4cde8205
Leo Leite <leomagalhaes2008@gmail.com>
2017-10-12 23:13:48 +0000

    Migrations ready?(Fix)

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from flask.ext.migrate import Migrate
 app = Flask(__name__)
 app.config.from_object('settings')
 db = SQLAlchemy(app)
-migrate = Migrate(db,app)
+migrate = Migrate(app, db)
 
 from blog import views
 from author import views
