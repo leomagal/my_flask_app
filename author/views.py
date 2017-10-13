9cafc8b00fd1f80727ae995a5bf5e8b171be2b26
Leo Leite <leomagalhaes2008@gmail.com>
2017-10-13 21:16:18 +0000

    Minor fixes (3)

--- author/views.py
+++ author/views.py
@@ -1,6 +1,8 @@
 from my_blog import app
-from flask import render_template, redirect, url_for
+from my_blog import db
+from flask import render_template, redirect, url_for, flash
 from author.form import RegisterForm
+from author.models import Author
 
 @app.route('/login')
 def login():
@@ -9,8 +11,20 @@ def login():
 @app.route('/register', methods = ('GET', 'POST'))
 def register():
     form = RegisterForm()
+    error=None
     if form.validate_on_submit():
-        return redirect(url_for('success'))
+        author= Author(form.fullname.data,
+        form.email.data,
+        form.username.data,
+        form.password.data,
+        True
+        )
+        db.session.add(author)
+        db.session.flush()
+        if author.id:
+            db.session.commit()
+            flash("Author registered")
+            return redirect(url_for('success'))
     return render_template('author/register.html', form=form)
 
 @app.route('/success')
