97b96f182e1142d3a1d6ca881d65ec91a373c3a8
Leo Leite <leomagalhaes2008@gmail.com>
2017-10-13 20:53:12 +0000

    Minor fixes (2)

--- blog/views.py
+++ blog/views.py
@@ -17,7 +17,33 @@ def admin():
         return redirect(url_for('setup'))
     return render_template('blog/admin.html')
 
-@app.route('/setup')
+@app.route('/setup', methods=('GET','POST'))
 def setup():
     form=SetupForm()
-    return render_template('blog/setup.html', form=form)
+    error=None
+    if form.validate_on_submit():
+        author= Author(form.fullname.data,
+        form.email.data,
+        form.username.data,
+        form.password.data,
+        True
+        )
+        db.session.add(author)
+        db.session.flush()
+        if author.id:
+            blog = Blog(form.name.data, 
+            author.id
+            )
+            db.session.add(blog)
+            db.session.flush()
+        else:
+            db.sesssion.rollback()
+            error = "Error creating user"
+        if author.id and blog.id:
+            db.session.commit()
+            flash("Blog created")
+            return redirect(url_for('admin'))
+        else:
+            db.session.rollback()
+            error= "Error creating blog"
+    return render_template('blog/setup.html', form=form, error=error)
