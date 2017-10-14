015d72b4beccaa7e9f8510b14280d18b0b3f4aa1
Leo Leite <leomagalhaes2008@gmail.com>
2017-10-14 08:10:06 +0000

    Handle DB errors at author registration. Add ul_flash(error) macro.

--- author/views.py
+++ author/views.py
@@ -1,6 +1,7 @@
 from my_blog import app
 from my_blog import db
 from flask import render_template, redirect, url_for, flash
+from sqlalchemy.exc import IntegrityError
 from author.form import RegisterForm
 from author.models import Author
 
@@ -11,7 +12,8 @@ def login():
 @app.route('/register', methods = ('GET', 'POST'))
 def register():
     form = RegisterForm()
-    error=None
+    error = None
+    #get_flashed_messages()
     if form.validate_on_submit():
         author= Author(form.fullname.data,
         form.email.data,
@@ -19,13 +21,33 @@ def register():
         form.password.data,
         True
         )
+        print("\n***\n Forms validated, querying database \n***")
         db.session.add(author)
-        db.session.flush()
-        if author.id:
-            db.session.commit()
-            flash("Author registered")
-            return redirect(url_for('success'))
-    return render_template('author/register.html', form=form)
+        print("\n***Added user to db Session***\n")
+        try:
+            db.session.flush()
+            print("\n ***\nSession Flushed\n ***")
+            if author.id:
+                db.session.commit()
+                print("\n ***\nSession Commited\n ***")
+                flash("Author registered")
+                return redirect(url_for('success'))
+        except IntegrityError as e:
+            error = "DB Error: "+str(e.args[0][1:27])+(" ")+str(e.args[0][30:34])
+            if error == "DB Error: pymysql.err.IntegrityError 1062":
+                print("\n\n*** %s ***\n\n"%error)
+                flash("Try different username or email")
+                db.session.rollback()
+                db.session.close()
+                return render_template('author/register.html', form=form, error=error)
+            else: 
+                error = "Unexpected IntegrityError: %s" % str(e.args)
+                flash(error)
+                db.session.rollback()
+                db.session.close()
+                return render_template('author/register.html', form=form, error=error)
+    else:
+        return render_template('author/register.html', form=form, error=error)
 
 @app.route('/success')
 def success():
